bee41c21312bc1e6c125ec8193493b9f269210bf
myscience <muratore.paolo95@gmail.com>
2024-05-09 18:54:25 +0200

    🚧: Refactor CausalConv1d and BlockLinear classes

--- xlstm/utils.py
+++ xlstm/utils.py
@@ -1,8 +1,28 @@
 import torch
 import torch.nn as nn
 
+from einops import rearrange
+
 from torch import Tensor
-from typing import List
+from typing import List, TypeVar
+
+T = TypeVar('T')
+D = TypeVar('D')
+
+def exists(var : T | None) -> bool:
+    return var is not None
+
+def default(var : T | None, val : D) -> T | D:
+    return var if exists(var) else val
+
+def enlarge_as(src : Tensor, other : Tensor) -> Tensor:
+    '''
+        Add sufficient number of singleton dimensions
+        to tensor a **to the right** so to match the
+        shape of tensor b. NOTE that simple broadcasting
+        works in the opposite direction.
+    '''
+    return rearrange(src, f'... -> ...{" 1" * (other.dim() - src.dim())}').contiguous()
 
 class CausalConv1d(nn.Conv1d):
     def __init__(
@@ -15,37 +35,50 @@ class CausalConv1d(nn.Conv1d):
         groups=1,
         bias=True
     ):
-        self.__padding = (kernel_size - 1) * dilation
+        self._padding = (kernel_size - 1) * dilation
 
         super(CausalConv1d, self).__init__(
             in_channels,
             out_channels,
             kernel_size=kernel_size,
             stride=stride,
-            padding=self.__padding,
+            padding=self._padding,
             dilation=dilation,
             groups=groups,
             bias=bias)
 
-    def forward(self, input : Tensor) -> Tensor:
-        result = super(CausalConv1d, self).forward(input)
-        if self.__padding != 0: return result[:, :, :-self.__padding]
+    def forward(self, inp : Tensor) -> Tensor:
+        # Handle the case where input has only two dimensions
+        # we expect them to have semantics (batch, channels),
+        # so we add the missing dimension manually
+        if inp.dim() == 2: inp = rearrange(inp, 'b i -> b 1 i')
+        
+        result = super(CausalConv1d, self).forward(inp)
+        if self._padding != 0: return result[..., :-self._padding]
         return result
     
 class BlockLinear(nn.Module):
     def __init__(
         self,
-        block_dims : List[int],
+        block_dims : List[int | List[int]],
+        bias : bool = False,
     ):
         super(BlockLinear, self).__init__()
         
         self._blocks = nn.ParameterList([
-            nn.Parameter(torch.randn(size, size, requires_grad=True))
+            nn.Parameter(torch.randn(size, requires_grad=True))
             for size in block_dims
         ])
+        
+        self._bias = nn.Parameter(torch.zeros(sum(block_dims))) if bias else None
         
     def forward(self, inp : Tensor) -> Tensor:
         # Assemble the blocks into a block-diagonal matrix
         full = torch.block_diag(*self._blocks)
         
-        return torch.matmul(full, inp)
+        out = torch.matmul(full, inp)
+        
+        if self._bias is not None:
+            out = out + self._bias
+        
+        return out
