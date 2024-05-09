44851180906fbec8af85846ad2ffca0c443ed495
myscience <muratore.paolo95@gmail.com>
2024-05-09 12:55:41 +0200

    🐛 Fix some minor bugs in sLSTM implementation, added test and updated gitignore

---
+++ test/test_slstm.py
@@ -0,0 +1,57 @@
+import unittest
+import torch
+from xlstm import sLSTM
+
+class TestSLSTM(unittest.TestCase):
+    def setUp(self):        
+        self.inp_dim = 10
+        self.head_dim = 8
+        self.head_num = 4
+        self.hid_dim = self.head_num * self.head_dim
+        
+        self.batch_size = 5
+        
+        self.model = sLSTM(self.inp_dim, self.head_dim, self.head_num)
+        self.input = torch.randn(self.batch_size, self.inp_dim)
+        
+        self.hid_0 = self.model.init_hidden()
+
+    def test_output_shape(self):
+        output, _ = self.model(self.input, self.hid_0)
+        
+        self.assertEqual(output.shape, (self.batch_size, self.inp_dim))
+
+    def test_hidden_shape(self):
+        hid = self.model.init_hidden()
+        self.assertEqual(len(hid), 4) 
+        
+        self.assertEqual(hid[0].shape, (self.hid_dim,))
+        self.assertEqual(hid[1].shape, (self.hid_dim,))
+        self.assertEqual(hid[2].shape, (self.hid_dim,))
+        self.assertEqual(hid[3].shape, (self.hid_dim,))
+
+    def test_forward_no_conv(self):
+        output, _ = self.model(self.input, self.hid_0)
+        self.assertEqual(output.shape, (self.batch_size, self.inp_dim))
+        
+    def test_forward_with_conv(self):
+        output, _ = self.model(self.input, self.hid_0, use_conv=True)
+        self.assertEqual(output.shape, (self.batch_size, self.inp_dim))
+
+    def test_backward(self):
+        criterion = torch.nn.MSELoss()
+        
+        target = torch.randn(self.batch_size, self.inp_dim)
+        output, _ = self.model(self.input, self.hid_0)
+        
+        loss = criterion(output, target)
+        loss.backward()
+
+        # Check if gradients are computed for all parameters
+        # with the possible exception of the causal conv
+        for name, param in self.model.named_parameters():
+            if 'causal_conv' in name: continue
+            self.assertIsNotNone(param.grad)
+
+if __name__ == '__main__':
+    unittest.main()
--- xlstm/lstm.py
+++ xlstm/lstm.py
@@ -27,25 +27,29 @@ class sLSTM(nn.Module):
     
     def __init__(
         self,
-        seq_dim : int,
+        inp_dim : int,
         head_dim : int,
         head_num : int,
     ) -> None:
         super().__init__()
         
-        self.inp_norm = nn.LayerNorm(seq_dim)
-        self.hid_norm = nn.GroupNorm(head_num, head_dim)
+        self.inp_dim = inp_dim
+        self.head_dim = head_dim
+        self.head_num = head_num
+        
+        self.inp_norm = nn.LayerNorm(inp_dim)
+        self.hid_norm = nn.GroupNorm(head_num, head_dim * head_num)
         
         self.causal_conv = CausalConv1d(
-            seq_dim,
-            seq_dim,
+            inp_dim,
+            inp_dim,
             kernel_size=4
         )
         
-        self.W_z = nn.Linear(seq_dim, head_num * head_dim)
-        self.W_i = nn.Linear(seq_dim, head_num * head_dim)
-        self.W_o = nn.Linear(seq_dim, head_num * head_dim)
-        self.W_f = nn.Linear(seq_dim, head_num * head_dim)
+        self.W_z = nn.Linear(inp_dim, head_num * head_dim)
+        self.W_i = nn.Linear(inp_dim, head_num * head_dim)
+        self.W_o = nn.Linear(inp_dim, head_num * head_dim)
+        self.W_f = nn.Linear(inp_dim, head_num * head_dim)
         
         self.R_z = BlockLinear([head_dim] * head_num)
         self.R_i = BlockLinear([head_dim] * head_num)
@@ -58,17 +62,36 @@ class sLSTM(nn.Module):
         # See Fig. 9 in the paper.
         proj_dim = int(4/3 * head_num * head_dim)
         self.up_proj   = nn.Linear(head_num * head_dim, 2 * proj_dim)
-        self.down_proj = nn.Linear(proj_dim, head_num * head_dim)
+        self.down_proj = nn.Linear(proj_dim, inp_dim)
+        
+    def init_hidden(self) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+        '''Initialize the hidden state of the sLSTM model.
+
+        Args:
+            batch_size (int): The batch size of the input sequence.
+
+        Returns:
+            Tuple[Tensor, Tensor, Tensor, Tensor]: The hidden state tuple containing the cell state,
+                normalizer state, hidden state, and stabilizer state.
+        '''
+        
+        n_0 = torch.ones (self.head_num * self.head_dim)
+        c_0 = torch.zeros(self.head_num * self.head_dim)
+        h_0 = torch.zeros(self.head_num * self.head_dim)
+        m_0 = torch.zeros(self.head_num * self.head_dim)
+        
+        return c_0, n_0, h_0, m_0
         
     def forward(
         self,
         seq: Tensor,
-        hid: Tuple[Tensor, Tensor, Tensor, Tensor],    
+        hid: Tuple[Tensor, Tensor, Tensor, Tensor],
+        use_conv : bool = False,    
     ) -> Tuple[Tensor, Tuple[Tensor, Tensor, Tensor, Tensor]]:
         '''Forward pass of the sLSTM model.
 
         Args:
-            seq (Tensor): The input sequence tensor of shape (batch_size, sequence_length, input_dim).
+            seq (Tensor): The input sequence tensor of shape (batch_size, input_dim).
             hid (Tuple[Tensor, Tensor, Tensor, Tensor]): The hidden state tuple containing the cell state,
                 normalizer state, hidden state, and stabilizer state.
 
@@ -77,18 +100,21 @@ class sLSTM(nn.Module):
                 connection and the newly updated hidden state tuple.
         '''
         
-        b, l, d = seq.shape
+        b, d = seq.shape
         
         # Separate the hidden (previous) state into the cell state,
         # the normalizer state, the hidden state, and the stabilizer state.
         c_tm1, n_tm1, h_tm1, m_tm1 = hid
         
-        x_t = self.inp_norm(seq)
+        x_t : Tensor = self.inp_norm(seq)
         
         # Optional causal convolution block for the input
         # and forget gates. See Fig. 9 in the paper.
-        x_c = self.causal_conv(x_t)
-        x_c = silu(x_c)
+        if use_conv:
+            x_c = self.causal_conv(x_t)
+            x_c = silu(x_c)
+        else:
+            x_c = x_t
         
         # Project the input to the different heads for all
         # the gates.
@@ -118,7 +144,7 @@ class sLSTM(nn.Module):
         
         # Perform up-and-down projection of the output with
         # projection factor 4/3. See Fig. (9) in supp. mat.
-        out1, out2 = self.up_proj(out).chunk(2)
+        out1, out2 = self.up_proj(out).chunk(2, dim=-1)
         
         out = out1 + gelu(out2)
         out = self.down_proj(out)
